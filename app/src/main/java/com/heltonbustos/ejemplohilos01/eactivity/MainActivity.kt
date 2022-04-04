d1f2cd4933c683a79c3a79a029a124af89675a3d
Helton Bustos <heltonsmith@hotmail.com>
2022-04-04 19:44:31 -0400

    se agregó barra horizontal y botón para descargar imagen y mostrarla en ImageView

--- app/src/main/java/com/heltonbustos/ejemplohilos01/eactivity/MainActivity.kt
+++ app/src/main/java/com/heltonbustos/ejemplohilos01/eactivity/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.heltonbustos.ejemplohilos01.eactivity
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.heltonbustos.ejemplohilos01.R
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     lateinit var txtHilo1:TextView
     lateinit var progressBar: ProgressBar
+    lateinit var progressBar2: ProgressBar
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -22,11 +24,14 @@ class MainActivity : AppCompatActivity() {
 
         txtHilo1 = findViewById(R.id.txtHilo1)
         progressBar = findViewById(R.id.progressBar)
+        progressBar2 = findViewById(R.id.progressBar2)
+        imageView = findViewById(R.id.imageView)
 
         var btnIniciarHiloBloqueado:Button = findViewById(R.id.btnIniciarHiloBloqueado)
         var btnIniciarHiloSegundoPlano:Button = findViewById(R.id.btnIniciarHiloSegundoPlano)
         var btnIniciarHiloSegundoPlanoUI:Button = findViewById(R.id.btnIniciarHiloSegundoPlanoUI)
         var btnIniciarHiloSegundoPlanoAsyncTask:Button = findViewById(R.id.btnIniciarHiloSegundoPlanoAsyncTask)
+        var btnDescargarImagen:Button = findViewById(R.id.btnDescargarImagen)
 
         btnIniciarHiloBloqueado.setOnClickListener {
             bloquearHilo()
@@ -43,6 +48,11 @@ class MainActivity : AppCompatActivity() {
         btnIniciarHiloSegundoPlanoAsyncTask.setOnClickListener {
             var hilo = Hilo()
             hilo.execute()
+        }
+
+        btnDescargarImagen.setOnClickListener {
+            DescargarImagen().execute(
+                "https://besthqwallpapers.com/Uploads/23-1-2021/153540/thumb2-skyline-cityscapes-4k-fantasy-art-comets-skyscrapers.jpg")
         }
 
     }
@@ -81,25 +91,65 @@ class MainActivity : AppCompatActivity() {
         h.start()
     }
 
+    /**
+     * Hilo de progressBar horizontal (barra)
+     */
     inner class Hilo: AsyncTask<Int, Int, String>() {
         override fun onPreExecute() {
+            progressBar.progress = 0
+            progressBar.max = 100
             super.onPreExecute()
         }
 
-        override fun doInBackground(vararg p0: Int?): String {
+        override fun doInBackground(vararg p0: Int?): String? {
+            //consultar la bd
             txtHilo1.text = "Hilo Inicia..."
-            publishProgress()
-            return ""
+            for(i in 0.. 100){
+                publishProgress(i)
+                Thread.sleep(10)
+            }
+            return "COMPLETADO"
         }
 
         override fun onProgressUpdate(vararg values: Int?) {
-            progressBar.visibility = View.VISIBLE
+            progressBar.progress = values[0]!!
             super.onProgressUpdate(*values)
         }
 
         override fun onPostExecute(result: String?) {
-            progressBar.visibility = View.GONE
-            Toast.makeText(applicationContext, "COMPLETADO", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+            super.onPostExecute(result)
+        }
+    }
+
+    /**
+     * Hilo de descargar imagen
+     */
+    inner class DescargarImagen: AsyncTask<String, Int, Bitmap>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg p0: String?): Bitmap? {
+            //consultar la bd
+            txtHilo1.text = "Descarga de la imagen Inicia..."
+            publishProgress()
+            val url = URL(p0[0])
+            val input = url.openStream()
+            val imagen: Bitmap = BitmapFactory.decodeStream(input)
+
+            return imagen
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            progressBar2.visibility = View.VISIBLE
+            super.onProgressUpdate(*values)
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            progressBar2.visibility = View.GONE
+            imageView.setImageBitmap(result)
+            Toast.makeText(applicationContext, "Imagen descargada OK", Toast.LENGTH_SHORT).show()
             super.onPostExecute(result)
         }
     }
